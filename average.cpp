2043a8827395a6f7080a5dd424ffc60e3b025622
adge <apiwit@gmail.com>
2023-03-03 15:15:27 +0700

    complete

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc,char *argv[]){
+  double sum =0 ;
+  if(argc==1){
+    cout << "Please input numbers to find average.\n" ;
+  }
+  else{
+    for(int i =0;i <argc;i++){
+        sum +=atoi(argv[i]);
+     
+}
+
+cout << "---------------------------------\n" ;
+cout << "Average of "<< argc-1 <<" numbers = " << sum/(argc-1) <<"\n" ; 
+ cout <<"---------------------------------\n" ;
+  }
+}
+   
 
